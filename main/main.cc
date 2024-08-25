2426affba1968aaabec115623c0edda538b53c59
AARM <arm@la-becaria.com>
2024-08-24 21:32:01 -0700

    Added Session class. Server is still not working

--- main/main.cc
+++ main/main.cc
@@ -7,6 +7,20 @@
 namespace beast = boost::beast;
 namespace net = boost::asio;
 
+class Session: public std::enable_shared_from_this<Session> {
+    net::ip::tcp::socket _socket;
+
+    public:
+        explicit Session(
+            net::ip::tcp::socket socket
+        ): 
+            _socket(std::move(socket)) {
+        }
+
+        void run() {
+        }
+};
+
 class Listener: public std::enable_shared_from_this<Listener> {
     net::io_context& ioc_;
     net::ip::tcp::acceptor acceptor_;
@@ -19,11 +33,29 @@ class Listener: public std::enable_shared_from_this<Listener> {
         beast::error_code ec;
         acceptor_.open(endpoint.protocol(), ec);
         if (ec) {
-            std::cerr << "Open error" << ec.message() << std::endl;
+            std::cerr << "Open error: " << ec.message() << std::endl;
             return;
         }
 
         acceptor_.set_option(net::socket_base::reuse_address(true), ec);
+        if (ec) {
+            std::cerr << "Set option error: " << ec.message() << std::endl;
+            return;
+        }
+        
+        acceptor_.bind(endpoint, ec);
+        if (ec) {
+            std::cerr << "Bind error: " << ec.message() << std::endl;
+            return;
+        }
+
+        acceptor_.listen(net::socket_base::max_listen_connections, ec);
+        if (ec) {
+            std::cerr << "Listen error: " << ec.message() << std::endl;
+            return;
+        }
+
+        do_accept();
     }
     
     static std::shared_ptr<Listener> create(
@@ -36,6 +68,19 @@ class Listener: public std::enable_shared_from_this<Listener> {
     std::shared_ptr<Listener> getptr() {
         return shared_from_this();
     }
+ 
+    private:
+        void do_accept() {
+            acceptor_.async_accept(
+                net::make_strand(ioc_), 
+                [this](beast::error_code ec, net::ip::tcp::socket socket) {
+                    if(!ec) {
+                        std::make_shared<Session>(std::move(socket))->run();
+                    } 
+                    do_accept();
+                }
+            );
+        }
 };
 
 int main() {
